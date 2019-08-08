09549fc65435f378695cc98e9533e9c1acf7f21c
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2019-08-08 17:44:32 +0200

    Add commandline options to auth script for more flexibility

--- pf_auth/pf_auth.sh
+++ pf_auth/pf_auth.sh
@@ -2,15 +2,46 @@
 
 THIS_DIR="$(cd "$(/usr/bin/dirname "$0")" || exit 1; /bin/pwd)"
 
-PF_TBL=${PF_TBL:-"tbl_sshauth_block"}
 FILTER=${FILTER:-"$THIS_DIR/filter_ipaddr.py"}
 PYTHON=${PYTHON:-"/usr/local/bin/python2"}
+
+NUMBER=${NUMBER:-"5"}
 EXPIRE=${EXPIRE:-"86400"}
+PF_TBL=${PF_TBL:-"tbl_sshauth_block"}
+
+# writes message to stderr
+_msg() { >&2 echo "$*"; }
+
+# shows error message and quits
+_fatal() { _msg "$*"; exit 1; }
+
+# shows usage information and quits
+_usage() {
+    _msg "usage: $0 [-n] [-e] [-t] [-h] auth [auth [...]]"
+    _msg "  -n  addresses with min. occurrences"
+    _msg "  -e  pf expire time in seconds"
+    _msg "  -t  pf table name"
+    _msg "  -h  show this help and quit"
+    _msg "auth  path to auth.log file(s)"
+    [ -n "$*" ] && _fatal "$*"
+    exit 0
+}
+
+# parse arguments
+while getopts ":t:e:n:h" OPT "$@"; do
+    case $OPT in
+        n)  NUMBER="$OPTARG"                    ;;
+        e)  EXPIRE="$OPTARG"                    ;;
+        t)  PF_TBL="$OPTARG"                    ;;
+        h)  _usage                              ;;
+        :)  _usage "-$OPTARG needs an argument" ;;
+        \?) _usage "invalid option: -$OPTARG"   ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
 
 AUTH_LOGS="$*"
 
-# shows error message and quits
-_fatal() { >&2 echo "$*"; exit 1; }
 
 # check if pf is available - e.g. while booting
 [ ! -e "/dev/pf" ] && _fatal "pf is not available"
@@ -24,15 +55,15 @@ for AUTH_LOG in $AUTH_LOGS; do
 done
 
 # grep all failed sshd connections from the logs
-# collect all addresses that occur more than five times
+# collect all addresses that occur more than $NUMBER times
 # and add them to the table
 for AUTH_LOG in $AUTH_LOGS; do
     /usr/bin/grep -ie 'sshd.*failed' "$AUTH_LOG" |
-    $PYTHON "$FILTER" -a 5 |
+    $PYTHON "$FILTER" -a "$NUMBER" |
     /sbin/pfctl -q -t "$PF_TBL" -T add -f -
 done
 
-# remove old entries
+# remove expired entries
 /sbin/pfctl -q -t "$PF_TBL" -T expire "$EXPIRE"
 
 exit 0
