176a93dbcfd9e59eeb35f38310077c4faf41031e
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2020-03-22 18:01:13 +0100

    Add some simple script to collect recurring output in a logfile

---
+++ pipe_log/pipe_log.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+THIS_DIR="$(cd "$(/usr/bin/dirname "$0")" || exit 1; /bin/pwd)"
+
+TARGET="$THIS_DIR/pipe.log"
+[ -n "$1" ] && TARGET="$1"
+[ ! -f "$TARGET" ] && :> "$TARGET"
+
+STAMP=$(date '+%Y-%m-%d %H-%M-%S')
+
+if [ -t 0 ]; then
+    printf ">>> %s <<<\n" "$STAMP"
+    while IFS= read -r LINE; do
+        printf "%s\n" "$LINE"
+    done < "$TARGET"
+else
+    {
+        printf ">   %s   <\n" "$STAMP"
+        while IFS= read -r LINE; do
+            printf "%s\n" "$LINE"
+        done
+        printf "\n"
+    } >> "$TARGET"
+fi
+
+exit 0
---
+++ pipe_log/readme.md
@@ -0,0 +1,28 @@
+# pipe log
+
+> collect output of high frequency (cron) jobs and generate summary
+
+
+## Remarks
+
+Redirection is difficult, so only one pipe is used.
+Do not forget about ``2>&1`` if errors should end up in the log file.
+
+
+## Usage
+
+* Create multiple cron jobs, at least two
+    * At least one to collect data
+    * One to output generated summary
+
+Collect:
+
+```
+/some/command 2>&1 | /some/where/pipe_log/pipe_log.sh "/what/ever.log"
+```
+
+Output:
+
+```
+/some/where/pipe_log/pipe_log.sh "/what/ever.log"
+```
