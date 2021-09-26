3c257173c70d7451297e8b7ec99e57a271711fd0
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2021-09-26 14:53:11 +0200

    Hack together some script to keep package lists for all machines

---
+++ jail_pkg_list/jail_pkg_list.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+TARGET="${TARGET:-"/root/.pkg_list"}"
+
+msg_head() {
+    echo "###"
+    echo "# $*"
+}
+msg_code() {
+    echo "# $2 -> $1"
+    echo "###"
+    echo
+}
+
+action() {
+    NAME="$1"; shift
+    msg_head "$NAME"
+    LIST="$TARGET/list_$NAME.txt"
+    /usr/sbin/pkg "$@" info -ao > "$LIST"
+    msg_code "$?" "$LIST"
+}
+
+
+[ ! -d "$TARGET" ] && {
+    msg_head "Creating folder" "$TARGET"
+    mkdir -p "$TARGET"
+    msg_code "$?" "$TARGET"
+}
+
+action "host"
+for NAME in $(/usr/sbin/jls -q name); do
+    action "$NAME" -j "$NAME"
+done
+
+exit 0
---
+++ jail_pkg_list/readme.md
@@ -0,0 +1,10 @@
+# jail_pkg_list
+
+> store current package listing from host and all jails
+
+Great for running ``pkg info -ao`` on everything
+
+## Usage
+
+* Symlink ``jail_pkg_list.sh`` to ``/root/bin/jail_pkg_list``
+* Run ``jail_pkg_list`` to get info listings inside `/root/.pkg_list`
