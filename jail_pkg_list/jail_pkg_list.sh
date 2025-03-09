b742a74119b78071b7cdaa1418eccf9cff98901f
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2025-03-09 13:22:17 +0100

    Make base name configurable and use less verbose output in jail_pkg_list

--- jail_pkg_list/jail_pkg_list.sh
+++ jail_pkg_list/jail_pkg_list.sh
@@ -1,35 +1,24 @@
 #!/usr/bin/env sh
 
 TARGET="${TARGET:-"$HOME/pkg_list"}"
+BASE="${BASE:-"host"}"
 
-msg_head() {
-    echo "###"
-    echo "# $*"
-}
-msg_code() {
-    echo "# $2 -> $1"
-    echo "###"
-    echo
-}
 
 action() {
     NAME="$1"; shift
-    msg_head "$NAME"
     LIST="$TARGET/$NAME.txt"
+    echo "#> $LIST"
     /usr/sbin/pkg "$@" info -ao > "$LIST"
-    msg_code "$?" "$LIST"
 }
 
 
 [ ! -d "$TARGET" ] && {
-    msg_head "Creating folder" "$TARGET"
+    echo "## Creating folder" "$TARGET"
     /bin/mkdir -p "$TARGET"
-    msg_code "$?" "$TARGET"
 }
 
+action "$BASE"
 for NAME in $(/usr/sbin/jls -q name | /usr/bin/sort); do
-action "host"
-for NAME in $(/usr/sbin/jls -q name); do
     action "$NAME" -j "$NAME"
 done
 
