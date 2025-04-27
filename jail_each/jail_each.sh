e31467814afcaca0402fc01084a4f9c9f777cdef
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2025-04-27 17:40:53 +0200

    Change jail_each to run on base host as well and set default to current user

--- jail_each/jail_each.sh
+++ jail_each/jail_each.sh
@@ -4,13 +4,15 @@
 _msg() { >&2 echo "$*"; }
 _fatal() { _msg "$*"; exit 1; }
 
-USERNAME="${USERNAME:-"root"}"
+USERNAME="${USERNAME:-"$(/usr/bin/id -un)"}"
+BASE_RUN=
 CONTINUE=
 
 # shows usage information and quits
 _usage() {
-    _msg "usage: $0 [-U username] [-c] [-h] cmd"
-    _msg "  -U  user from jailed environment - default is $USERNAME"
+    _msg "usage: $0 [-U username] [-b] [-c] [-h] cmd"
+    _msg "  -U  user in jail - default is $USERNAME"
+    _msg "  -b  also run command on base host (-U has no effect)"
     _msg "  -c  do not stop on failed command"
     _msg "  -h  show this help and quit"
     _msg "cmd   command to run"
@@ -19,9 +21,10 @@ _usage() {
 }
 
 # parse arguments
-while getopts ":U:ch" OPT "$@"; do
+while getopts ":U:bch" OPT "$@"; do
     case $OPT in
         U)  USERNAME="$OPTARG"                  ;;
+        b)  BASE_RUN=1                          ;;
         c)  CONTINUE=1                          ;;
         h)  _usage                              ;;
         :)  _usage "-$OPTARG needs an argument" ;;
@@ -32,17 +35,24 @@ shift $(( OPTIND - 1 ))
 
 [ -z "$*" ] && _fatal "command missing"
 
-
-for NAME in $(/usr/sbin/jls -q name | /usr/bin/sort); do
+_launch() {
+    NAME=$1; shift
     _msg "###"
     _msg "# $NAME"
-    /usr/sbin/jexec -U "$USERNAME" "$NAME" "$@"
+    "$@"
     CODE=$?
     _msg "# $NAME $CODE"
     _msg "###";
     [ $CODE -ne 0 ] && [ ! $CONTINUE ] && {
         _fatal ">> $NAME error"
     }
+}
+
+[ $BASE_RUN ] && {
+    _launch "base" "$@"
+}
+for NAME in $(/usr/sbin/jls -q name | /usr/bin/sort); do
+    _launch "$NAME" /usr/sbin/jexec -U "$USERNAME" "$NAME" "$@"
 done
 
 exit 0
--- jail_each/readme.md
+++ jail_each/readme.md
@@ -7,5 +7,6 @@ Great for running `pkg` or `freebsd-update` on everything
 ## Usage
 
 - Symlink `jail_each.sh` to `/root/bin/jail_each`
-- Run e.G. `jail_each pkg upgrade` to keep everything
+- Run e.G. `jail_each -b pkg upgrade` to keep everything
   up to date
+- See `jail_each -h` for options
